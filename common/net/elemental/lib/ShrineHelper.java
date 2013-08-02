d5bce806c3874fda1e050fd7d4bdb1d20d43861d
logomaster256 <marko.lazaric@gmail.com>
2013-08-02 02:26:08 +0200

    15 minute autocommit

---
+++ common/net/elemental/lib/ShrineHelper.java
@@ -0,0 +1,19 @@
+package net.elemental.lib;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+
+public class ShrineHelper 
+{
+	// Activator
+	public static final int ACTIVATOR_ITEM_ID = Item.stick.itemID;
+	public static final int ACTIVATOR_ITEM_META = -1;
+	
+	// Center block on which you use the activator to activate
+	public static final int CENTER_BLOCK_ID = Block.blockDiamond.blockID;
+	public static final int CENTER_BLOCK_META = -1;
+	
+	// Offset from the edge of the shrine
+	public static final int CENTER_BLOCK_X_OFFSET = 1;
+	public static final int 
+}
