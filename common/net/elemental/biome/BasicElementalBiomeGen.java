2c3f543dd6feec50fb237f02ee262a804ce26035
logomaster256 <marko.lazaric@gmail.com>
2013-08-03 18:19:07 +0200

    15 minute autocommit

--- common/net/elemental/biome/BasicElementalBiomeGen.java
+++ common/net/elemental/biome/BasicElementalBiomeGen.java
@@ -1,6 +1,17 @@
 package net.elemental.biome;
 
-import net.elemental.entity.passive.*;
+import java.util.Random;
+
+import net.elemental.block.Blocks;
+import net.elemental.entity.passive.EntityElementalBat;
+import net.elemental.entity.passive.EntityElementalChicken;
+import net.elemental.entity.passive.EntityElementalCow;
+import net.elemental.entity.passive.EntityElementalOcelot;
+import net.elemental.entity.passive.EntityElementalPig;
+import net.elemental.entity.passive.EntityElementalSheep;
+import net.elemental.entity.passive.EntityElementalSquid;
+import net.elemental.entity.passive.EntityElementalWolf;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeDecorator;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.biome.SpawnListEntry;
@@ -43,40 +54,6 @@ public class BasicElementalBiomeGen extends BiomeGenBase
 	{
 		return new ElementalBiomeDecorator(this);
 	}
-	
-	@SuppressWarnings("unchecked")
-	private void addCaveCreatures()
-	{
-        spawnableCaveCreatureList.add(new SpawnListEntry(EntityElementalBat.class, 10, 8, 8));
-	}
-
-	@SuppressWarnings("unchecked")
-	private void addMonsters()
-	{
-        //spawnableMonsterList.add(new SpawnListEntry(EntityElementalSpider.class, 10, 4, 4));
-        //spawnableMonsterList.add(new SpawnListEntry(EntityElementalZombie.class, 10, 4, 4));
-        //spawnableMonsterList.add(new SpawnListEntry(EntityElementalSkeleton.class, 10, 4, 4));
-        //spawnableMonsterList.add(new SpawnListEntry(EntityElementalCreeper.class, 10, 4, 4));
-        //spawnableMonsterList.add(new SpawnListEntry(EntityElementalSlime.class, 10, 4, 4));
-        //spawnableMonsterList.add(new SpawnListEntry(EntityElementalEnderman.class, 1, 1, 4));
-	}
-
-	@SuppressWarnings("unchecked")
-	private void addWaterCreatures()
-	{
-        spawnableWaterCreatureList.add(new SpawnListEntry(EntityElementalSquid.class, 10, 4, 4));
-    }
-
-	@SuppressWarnings("unchecked")
-	private void addCreatures()
-	{
-		spawnableCreatureList.add(new SpawnListEntry(EntityElementalSheep.class, 12, 4, 4));
-		spawnableCreatureList.add(new SpawnListEntry(EntityElementalPig.class, 10, 4, 4));
-		spawnableCreatureList.add(new SpawnListEntry(EntityElementalChicken.class, 15, 4, 4));
-		spawnableCreatureList.add(new SpawnListEntry(EntityElementalCow.class, 8, 4, 4));
-        spawnableCreatureList.add(new SpawnListEntry(EntityElementalWolf.class, 5, 4, 4));
-        spawnableMonsterList.add(new SpawnListEntry(EntityElementalOcelot.class, 2, 1, 1));
-	}
 
 	@Override
 	public int getSkyColorByTemp(float temp)
@@ -117,5 +94,63 @@ public class BasicElementalBiomeGen extends BiomeGenBase
 	public EnumBiomes getBiome()
 	{
 		return biome;
+	}
+	
+	@Override
+	public void decorate(World world, Random random, int x, int z)
+    {
+        super.decorate(world, random, x, z);
+        int k = 3 + random.nextInt(6);
+        int l;
+        int i1;
+        int j1;
+
+        for (l = 0; l < k; ++l)
+        {
+            i1 = x + random.nextInt(16);
+            j1 = random.nextInt(28) + 4;
+            int k1 = z + random.nextInt(16);
+            int l1 = world.getBlockId(i1, j1, k1);
+
+            if (l1 == Blocks.elementalStoneBlock.blockID)
+            {
+                world.setBlock(i1, j1, k1,
+                		Blocks.elementalOreBlock2.blockID, 4 + biome.ordinal(), 2);
+            }
+        }
+    }
+	
+	@SuppressWarnings("unchecked")
+	private void addCaveCreatures()
+	{
+        spawnableCaveCreatureList.add(new SpawnListEntry(EntityElementalBat.class, 10, 8, 8));
+	}
+
+	@SuppressWarnings("unchecked")
+	private void addMonsters()
+	{
+        //spawnableMonsterList.add(new SpawnListEntry(EntityElementalSpider.class, 10, 4, 4));
+        //spawnableMonsterList.add(new SpawnListEntry(EntityElementalZombie.class, 10, 4, 4));
+        //spawnableMonsterList.add(new SpawnListEntry(EntityElementalSkeleton.class, 10, 4, 4));
+        //spawnableMonsterList.add(new SpawnListEntry(EntityElementalCreeper.class, 10, 4, 4));
+        //spawnableMonsterList.add(new SpawnListEntry(EntityElementalSlime.class, 10, 4, 4));
+        //spawnableMonsterList.add(new SpawnListEntry(EntityElementalEnderman.class, 1, 1, 4));
+	}
+
+	@SuppressWarnings("unchecked")
+	private void addWaterCreatures()
+	{
+        spawnableWaterCreatureList.add(new SpawnListEntry(EntityElementalSquid.class, 10, 4, 4));
+    }
+
+	@SuppressWarnings("unchecked")
+	private void addCreatures()
+	{
+		spawnableCreatureList.add(new SpawnListEntry(EntityElementalSheep.class, 12, 4, 4));
+		spawnableCreatureList.add(new SpawnListEntry(EntityElementalPig.class, 10, 4, 4));
+		spawnableCreatureList.add(new SpawnListEntry(EntityElementalChicken.class, 15, 4, 4));
+		spawnableCreatureList.add(new SpawnListEntry(EntityElementalCow.class, 8, 4, 4));
+        spawnableCreatureList.add(new SpawnListEntry(EntityElementalWolf.class, 5, 4, 4));
+        spawnableMonsterList.add(new SpawnListEntry(EntityElementalOcelot.class, 2, 1, 1));
 	}
 }
