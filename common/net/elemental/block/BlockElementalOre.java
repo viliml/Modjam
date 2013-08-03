04b58a1131476d22ff373666550926540f6cce9b
logomaster256 <marko.lazaric@gmail.com>
2013-08-03 19:25:27 +0200

    15 minute autocommit

--- common/net/elemental/block/BlockElementalOre.java
+++ common/net/elemental/block/BlockElementalOre.java
@@ -49,21 +49,21 @@ public class BlockElementalOre extends Block {
 			for (i = 0; i < 4; ++i)
 				for (j = 0; j < 4; ++j)
 					icons[i * 4 + j] = iconRegister.registerIcon(
-						Reference.MOD_ID + ":" +
-							GeneralHelper.ELEMENTS[j].toLowerCase() + "_" +
-							GeneralHelper.ORES[i + 4].toLowerCase() + "_ore");
+							Reference.MOD_ID + ":" +
+									GeneralHelper.ELEMENTS[j].toLowerCase() + "_" +
+									GeneralHelper.ORES[i + 4].toLowerCase() + "_ore");
 		else
 			for (i = 0; i < 4; ++i)
 				for (j = 0; j < 4; ++j)
 					icons[i * 4 + j] = iconRegister.registerIcon(
-						Reference.MOD_ID + ":" +
-								GeneralHelper.ELEMENTS[j].toLowerCase() + "_" +
-								GeneralHelper.ORES[i].toLowerCase() + "_ore");
+							Reference.MOD_ID + ":" +
+									GeneralHelper.ELEMENTS[j].toLowerCase() + "_" +
+									GeneralHelper.ORES[i].toLowerCase() + "_ore");
 		//Register the active redstone icons. Simpler than to make an if in getIcon()
 		if (second)
 			for (i = 0; i < 4; i++) icons[12 + i] = icons[i];
 	}
-	
+
 	@Override
 	public Icon getIcon(int side, int meta)
 	{
@@ -74,7 +74,7 @@ public class BlockElementalOre extends Block {
 	@Override
 	public int getLightValue(IBlockAccess world, int x, int y, int z)
 	{
-		if (second && (world.getBlockMetadata(x, y, z) >> 3) == 3)
+		if (second && (world.getBlockMetadata(x, y, z) >> 2) == 3)
 			return 15;
 		return 0;
 	}
@@ -85,63 +85,107 @@ public class BlockElementalOre extends Block {
 		return meta;
 	}
 
-    /**
-     * How many world ticks before ticking
-     */
-    public int tickRate(World world)
-    {
-        return second ? 30 : 0;
-    }
+	@Override
+	public int tickRate(World world)
+	{
+		return second ? 30 : 0;
+	}
 
-    /**
-     * Called when the block is clicked by a player. Args: x, y, z, entityPlayer
-     */
-    public void onBlockClicked(World world, int x, int y, int z, EntityPlayer player)
-    {
-    	if (second && )
-        this.glow(world, x, y, z);
-        super.onBlockClicked(world, x, y, z, player);
-    }
+	@Override
+	public void onBlockClicked(World world, int x, int y, int z, EntityPlayer player)
+	{
+		glow(world, x, y, z);
+		super.onBlockClicked(world, x, y, z, player);
+	}
 
-    /**
-     * Called whenever an entity is walking on top of this block. Args: world, x, y, z, entity
-     */
-    public void onEntityWalking(World world, int x, int y, int z, Entity par5Entity)
-    {
-        this.glow(world, x, y, z);
-        super.onEntityWalking(world, x, y, z, par5Entity);
-    }
+	@Override
+	public void onEntityWalking(World world, int x, int y, int z, Entity par5Entity)
+	{
+		glow(world, x, y, z);
+		super.onEntityWalking(world, x, y, z, par5Entity);
+	}
 
-    /**
-     * Called upon block activation (right click on the block.)
-     */
-    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float par7, float par8, float par9)
-    {
-        this.glow(world, x, y, z);
-        return super.onBlockActivated(world, x, y, z, player, side, par7, par8, par9);
-    }
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float par7, float par8, float par9)
+	{
+		glow(world, x, y, z);
+		return super.onBlockActivated(world, x, y, z, player, side, par7, par8, par9);
+	}
 
-    /**
-     * The redstone ore glows.
-     */
-    private void glow(World world, int x, int y, int z)
-    {
-        this.sparkle(world, x, y, z);
+	private void glow(World world, int x, int y, int z)
+	{
+		sparkle(world, x, y, z);
 
-        if (this.blockID == Block.oreRedstone.blockID)
-        {
-            world.setBlock(x, y, z, Block.oreRedstoneGlowing.blockID);
-        }
-    }
+		if (second && (world.getBlockMetadata(x, y, z) >> 2) == 0)
+		{
+			world.setBlock(x, y, z, blockID, world.getBlockMetadata(x, y, z) + 12, 3);
+		}
+	}
 
-    /**
-     * Ticks the block if it's been scheduled
-     */
-    public void updateTick(World world, int x, int y, int z, Random random)
-    {
-        if (this.blockID == Block.oreRedstoneGlowing.blockID)
-        {
-            world.setBlock(x, y, z, Block.oreRedstone.blockID);
-        }
-    }
+	@Override
+	public void updateTick(World world, int x, int y, int z, Random random)
+	{
+		if (second && (world.getBlockMetadata(x, y, z) >> 2) == 3)
+		{
+			world.setBlock(x, y, z, blockID, world.getBlockMetadata(x, y, z) - 12, 3);
+		}
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void randomDisplayTick(World world, int x, int y, int z, Random par5Random)
+	{
+		if (second && (world.getBlockMetadata(x, y, z) >> 2) == 3)
+		{
+			sparkle(world, x, y, z);
+		}
+	}
+
+	private void sparkle(World world, int x, int y, int z)
+	{
+		Random random = world.rand;
+		double d0 = 0.0625D;
+
+		for (int l = 0; l < 6; ++l)
+		{
+			double d1 = (double)((float)x + random.nextFloat());
+			double d2 = (double)((float)y + random.nextFloat());
+			double d3 = (double)((float)z + random.nextFloat());
+
+			if (l == 0 && !world.isBlockOpaqueCube(x, y + 1, z))
+			{
+				d2 = (double)(y + 1) + d0;
+			}
+
+			if (l == 1 && !world.isBlockOpaqueCube(x, y - 1, z))
+			{
+				d2 = (double)(y + 0) - d0;
+			}
+
+			if (l == 2 && !world.isBlockOpaqueCube(x, y, z + 1))
+			{
+				d3 = (double)(z + 1) + d0;
+			}
+
+			if (l == 3 && !world.isBlockOpaqueCube(x, y, z - 1))
+			{
+				d3 = (double)(z + 0) - d0;
+			}
+
+			if (l == 4 && !world.isBlockOpaqueCube(x + 1, y, z))
+			{
+				d1 = (double)(x + 1) + d0;
+			}
+
+			if (l == 5 && !world.isBlockOpaqueCube(x - 1, y, z))
+			{
+				d1 = (double)(x + 0) - d0;
+			}
+
+			if (d1 < (double)x || d1 > (double)(x + 1) || d2 < 0.0D || d2 > (double)(y + 1) || d3 < (double)z || d3 > (double)(z + 1))
+			{
+				world.spawnParticle("reddust", d1, d2, d3, 0.0D, 0.0D, 0.0D);
+			}
+		}
+	}
 }
