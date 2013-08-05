d47adb455201eabe619adc6b956120e2b14f4b98
logomaster256 <marko.lazaric@gmail.com>
2013-08-05 19:14:44 +0200

    15 minute autocommit

--- common/net/elemental/lib/GeneralHelper.java
+++ common/net/elemental/lib/GeneralHelper.java
@@ -38,17 +38,16 @@ public class GeneralHelper
 	
 	public static void spawnParticles(String particleName, World world, int x, int y, int z)
 	{
-		if (particleName.equals("dirt"))
+		if (!particleName.equals("dirt"))
 		{
+			Random rand = new Random();
+			
+			for(int i = 0; i < (rand.nextInt(20) + 6); ++i)
+			{System.out.println(particleName + i);
+				world.spawnParticle(particleName, x + rand.nextDouble(), y + rand.nextDouble(), z + rand.nextDouble(), 0F, 0F, 0F);
+			}
+		}
+		else
 			Minecraft.getMinecraft().effectRenderer.addBlockDestroyEffects(x, y, z, Block.dirt.blockID, 0);
-			return;
-		}
-		
-		Random rand = new Random();
-		System.out.println(particleName);
-		for(int i = 0; i < (rand.nextInt(20) + 6); ++i)
-		{
-			world.spawnParticle(particleName, x + rand.nextDouble(), y + rand.nextDouble(), z + rand.nextDouble(), 0F, 0F, 0F);
-		}
 	}
 }
