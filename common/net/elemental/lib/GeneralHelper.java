eef6778ca751dc194ea23d90cf77aac4e5de63ac
logomaster256 <marko.lazaric@gmail.com>
2013-08-05 17:30:18 +0200

    15 minute autocommit

--- common/net/elemental/lib/GeneralHelper.java
+++ common/net/elemental/lib/GeneralHelper.java
@@ -2,6 +2,8 @@ package net.elemental.lib;
 
 import java.util.Random;
 
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.World;
 
 public class GeneralHelper 
@@ -26,8 +28,22 @@ public class GeneralHelper
 		"Active Redstone"
 	};
 	
+	public static final String[] PARTICLES = 
+	{
+		"dirt",
+		"smoke",
+		"flame",
+		"dripWater"
+	};
+	
 	public static void spawnParticles(String particleName, World world, int x, int y, int z)
 	{
+		if (particleName.equals("dirt"))
+		{
+			Minecraft.getMinecraft().effectRenderer.addBlockDestroyEffects(x, y, z, Block.dirt.blockID, 0);
+			return;
+		}
+		
 		Random rand = new Random();
 		
 		for(int i = 0; i < rand.nextInt(20); ++i)
--- common/net/elemental/tileentity/TileEntityBowl.java
+++ common/net/elemental/tileentity/TileEntityBowl.java
@@ -1,7 +1,31 @@
 package net.elemental.tileentity;
 
+import java.util.Random;
+
+import net.elemental.lib.GeneralHelper;
+import net.elemental.lib.ShrineHelper;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityBowl extends TileEntity
 {
+	private int timer = 50;
+	
+	@Override
+	public void updateEntity() 
+	{
+		if (timer > 0)
+		{
+			--timer;
+			return;
+		}
+		
+		ShrineHelper.buildNextBlock(this.worldObj, this.xCoord, this.yCoord, this.zCoord, GeneralHelper.PARTICLES[(new Random()).nextInt(4)]);
+		timer = 50;
+	}
+	
+	@Override
+	public boolean canUpdate()
+    {
+        return this.getBlockMetadata() == 0;
+    }
 }
