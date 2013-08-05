4ab0ff443213041cc356b5f676475b83b9a0792f
logomaster256 <marko.lazaric@gmail.com>
2013-08-05 20:51:02 +0200

    15 minute autocommit

--- common/net/elemental/tileentity/TileEntities.java
+++ common/net/elemental/tileentity/TileEntities.java
@@ -5,9 +5,11 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class TileEntities
 {
 	public static final String TE_BOWL_KEY = "tileEntityBowl";
+	public static final String TE_PORTAL_KEY = "tileEntityPortalElemental";
 
 	public static void initTileEntities()
 	{
 		GameRegistry.registerTileEntity(TileEntityBowl.class, TE_BOWL_KEY);
+		GameRegistry.registerTileEntity(TileEntityElementalPortal.class, TE_PORTAL_KEY);
 	}
 }
